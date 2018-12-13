08a53a84f32f288949082ad128c98f48977ae5c5
TowaGori <towa.doragon@gmail.com>
2018-12-13 10:15:47 +0900

    Aがキャラ移動処理を追加

--- YSD.java
+++ YSD.java
@@ -14,6 +14,9 @@ public class YSD extends Actor
      */
     public void act() 
     {
-        // Add your action code here.
+        if( Greenfoot.isKeyDown( "up" ) ){
+        setRotation(270);
+        move(1);
+       }// Add your action code here.
     }    
 }
