1d4e2fbb21cbce0e05b077a51aea84d3e5fc4106
渡邊 凜斗 <14199@jnet39-n.jnet.hakodate-ct.ac.jp>
2019-01-24 09:19:08 +0900

    データ取得

--- YSD.java
+++ YSD.java
@@ -15,43 +15,6 @@ public class YSD extends Actor
     public void act() 
     {
 
-        if( Greenfoot.isKeyDown( "down" ) ){
-        setRotation(90);
-        move(1);
-    }// Add your action code here.
-        if( Greenfoot.isKeyDown( "right" ) ){
-        setRotation(0);
-        move(1);
-    }// Add your action code here.
-    if( Greenfoot.isKeyDown( "right" ) ){
-        setRotation(0);
-        move(1);
-    }// Add your action code here.
-
-
-        if( Greenfoot.isKeyDown( "down" ) ){
-        setRotation(90);
-        move(1);
-}// Add your action code here.
-
-        if( Greenfoot.isKeyDown( "right" ) ){
-        setRotation(0);
-        move(1);
-}// Add your action code here.
-
-        if( Greenfoot.isKeyDown( "right" ) ){
-        setRotation(0);
-        move(1);
-    }// Add your action code here.
-
-        if( Greenfoot.isKeyDown( "up" ) ){
-        setRotation(270);
-        move(1);
-    }// Add your action code here.
-        if( Greenfoot.isKeyDown( "left" ) ){
-        setRotation(180);
-        move(1);
-    }// Add your action code here.
 }// Add your action code here. 
 
        }// Add your action code here.
--- game.java
+++ game.java
@@ -18,7 +18,7 @@ public class game extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1123, 794, 1); 
         
-        addObject( new YSD(), 100, 500 );
+        
         
     }
     public void act()
