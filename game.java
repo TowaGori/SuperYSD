24882a35f415e0146f39ca21f0e6914b04ceaa1c
13144 <13144@hakodate.kosen-ac.jp>
2019-01-31 09:19:14 +0900

    fujiyama

--- game.java
+++ game.java
@@ -17,15 +17,15 @@ public class game extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1123, 794, 1); 
-<<<<<<< HEAD
+
         addObject( new YSD(), 0, 0 );
         addObject( new Enemy(), 1100, 500 );
           showText( "ﾋｨｲｲｲｲｲｲｲ ", 500, 300 );
-=======
+
         
         
         
->>>>>>> 95aae1130b9818c9616660ef5fcae93ba496dc7f
+
     }
     public void act()
     {
